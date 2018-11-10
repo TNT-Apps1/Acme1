c9dec04807038078841c84936ba3a2243225ca75
TNT Apps <thompsongranth@yahoo.com>
2018-11-10 16:45:41 -0600

    initial commit

---
+++ Acme1/Controllers/ProductController.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using Acme1.Models;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Acme1.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        SqlConnection dbcon = new SqlConnection(ConfigurationManager.ConnectionStrings["Acmedb"].ConnectionString.ToString());
+
+
+        // GET: Product for index page list
+        public ActionResult Index()
+        {
+            List<Product> productList;
+            try
+            {
+                dbcon.Open();
+                productList = Product.GetProductList(dbcon, "");
+                dbcon.Close();
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+            return View(productList);
+
+        }
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            product.ImageFile = "nopic.jpg";
+            return View(product);
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Product product, HttpPostedFileBase uploadfile)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+                    if(uploadfile!= null && uploadfile.ContentLength > 0)
+                    {
+                        var fileName = Path.GetFileName(uploadfile.FileName);
+                        var path = Path.Combine(
+                        Server.MapPath("~/Content/Images/products"), fileName);
+                        uploadfile.SaveAs(path);
+                        product.ImageFile = fileName;
+                    }
+
+                    dbcon.Open();
+                    int intresult = Product.CUDProduct(dbcon, "create", product);
+                    dbcon.Close();
+                    return RedirectToAction("Index");
+
+                }
+                catch (Exception ex)
+                {
+                    if (dbcon.State == ConnectionState.Open)
+                    {
+                        dbcon.Close();
+                        ViewBag.errormsg = ex.Message;
+                        return View("_Error");
+                    }
+                
+            }
+            
+            }
+            ViewBag.errormsg = "Data validation error in Edit method";
+            return View("_Error");
+        }
+
+
+        // GET: Product
+        public ActionResult Edit(string id="")
+        {
+            if (Regex.IsMatch(id, @"^[a-zA-Z0-9]{2,10}$"))
+            {
+                try
+                {
+                    dbcon.Open();
+                    Product product = Product.GetProductSingle(dbcon, id);
+                    dbcon.Close();
+                    if (product.ProductID != null)
+                    {
+                        return View(product);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+
+
+            }
+            
+                ViewBag.errormsg = "Invalid data in the Edit Page";
+                return View("_Error");
+            
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product prod, HttpPostedFileBase uploadfile)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (uploadfile != null && uploadfile.ContentLength > 0)
+                    {
+                        var fileName = Path.GetFileName(uploadfile.FileName);
+                        var path = Path.Combine(
+                        Server.MapPath("~/Content/Images/products"), fileName);
+                        uploadfile.SaveAs(path);
+                        prod.ImageFile = fileName;
+                    }
+                    //Rest of Update code. Copy and modify the code
+                    //from the Post Edit action method in the mvc_db_tutorial.pdf here
+                    dbcon.Open();
+                    int intresult = Product.CUDProduct(dbcon, "update", prod);
+                    dbcon.Close();
+                    return RedirectToAction("Index");                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+            } //valid data
+            ViewBag.errmsg = "Data validation error in Edit method";
+            return View("Error");
+        }
+
+
+        // GET: Product
+        public ActionResult Delete(string id = "")
+        {
+            if (Regex.IsMatch(id, @"^[a-zA-Z0-9]{2,10}$"))
+            {
+                try
+                {
+                    dbcon.Open();
+                    Product product = Product.GetProductSingle(dbcon, id);
+                    dbcon.Close();
+                    if (product.ProductID != null)
+                    {
+                        return View(product);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+
+
+            }
+
+            ViewBag.errormsg = "Invalid data in the Edit Page";
+            return View("_Error");
+
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Product prod, HttpPostedFileBase uploadfile)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //if (uploadfile != null && uploadfile.ContentLength > 0)
+                    //{
+                    //    var fileName = Path.GetFileName(uploadfile.FileName);
+                    //    var path = Path.Combine(
+                    //    Server.MapPath("~/Content/Images/products"), fileName);
+                    //    uploadfile.SaveAs(path);
+                    //    prod.ImageFile = fileName;
+                    //}
+                    //Rest of Update code. Copy and modify the code
+                    //from the Post Edit action method in the mvc_db_tutorial.pdf here
+                    dbcon.Open();
+                    int intresult = Product.CUDProduct(dbcon, "delete", prod);
+                    dbcon.Close();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex) { throw new Exception(ex.Message); }
+            } //valid data
+            ViewBag.errmsg = "Data validation error in Edit method";
+            return View("Error");
+        }
+
+
+    }
+}
---
+++ Acme1/Models/Product.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+
+namespace Acme1.Models
+{
+    public class Product
+    {
+        [Required, Key, StringLength(10)]
+        //[RegularExpression("^[a-zA-Z0-9]{2-10}$", ErrorMessage = "Product ID is not valid")]
+        public string ProductID { get; set; }
+        [Required, Key, StringLength(20)]
+        public string Name { get; set; }
+        [Required, Key, StringLength(100)]
+        public string ShortDescription { get; set; }
+        [Required, Key, StringLength(200)]
+        [DataType(DataType.MultilineText)]
+        public string LongDescription { get; set; }
+        [Required, Key, StringLength(10)]
+        public string CategoryID { get; set; }
+        [Required, Key, StringLength(30)]
+        public string ImageFile { get; set; }
+        [Required, Range(maximum:999.99, minimum:.01)]
+        public decimal UnitPrice { get; set; }
+        [Required, Range(maximum: 999, minimum: 0)]
+        public int OnHand { get; set; }
+
+        public string imageFileToString
+        {
+            get
+            {
+                return this.ImageFile.Replace(".jpg", "");
+            }
+        }
+
+        public static Product GetProductSingle(SqlConnection dbcon, string id)
+        {
+            Product obj = new Product();
+            string strsql = "select * from Products where ProductID = '" + id + "'";
+            SqlCommand cmd = new SqlCommand(strsql, dbcon);
+            SqlDataReader rdr;
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                //put object data property assignment statements here
+                if (rdr["ProductID"] != DBNull.Value) obj.ProductID = rdr["ProductID"].ToString();
+                if (rdr["Name"] != DBNull.Value) obj.Name = rdr["Name"].ToString();
+                if (rdr["ShortDescription"] != DBNull.Value) obj.ShortDescription = rdr["ShortDescription"].ToString();
+                if (rdr["LongDescription"] != DBNull.Value) obj.LongDescription = rdr["LongDescription"].ToString();
+                if (rdr["CategoryID"] != DBNull.Value) obj.CategoryID = rdr["CategoryID"].ToString();
+                if (rdr["ImageFile"] != DBNull.Value) obj.ImageFile = rdr["ImageFile"].ToString();
+                if (rdr["UnitPrice"] != DBNull.Value) obj.UnitPrice = Convert.ToDecimal(rdr["UnitPrice"].ToString());
+                if (rdr["OnHand"] != DBNull.Value) obj.OnHand = Convert.ToInt32(rdr["OnHand"].ToString());
+            }
+            rdr.Close();
+            return obj;
+        }
+        public static List<Product> GetProductList(SqlConnection dbcon, string SqlClause)
+        {
+            List<Product> itemlist = new List<Product>();
+            string strsql = "select * from Products " + SqlClause;
+            SqlCommand cmd = new SqlCommand(strsql, dbcon);
+            SqlDataReader rdr;
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Product obj = new Product();
+                //put object data property assignment statements here
+                if (rdr["ProductID"] != DBNull.Value) obj.ProductID = rdr["ProductID"].ToString();
+                if (rdr["Name"] != DBNull.Value) obj.Name = rdr["Name"].ToString();
+                if (rdr["ShortDescription"] != DBNull.Value) obj.ShortDescription = rdr["ShortDescription"].ToString();
+                if (rdr["LongDescription"] != DBNull.Value) obj.LongDescription = rdr["LongDescription"].ToString();
+                if (rdr["CategoryID"] != DBNull.Value) obj.CategoryID = rdr["CategoryID"].ToString();
+                if (rdr["ImageFile"] != DBNull.Value) obj.ImageFile = rdr["ImageFile"].ToString();
+                if (rdr["UnitPrice"] != DBNull.Value) obj.UnitPrice = Convert.ToDecimal(rdr["UnitPrice"].ToString());
+                if (rdr["OnHand"] != DBNull.Value) obj.OnHand = Convert.ToInt32(rdr["OnHand"].ToString());
+                itemlist.Add(obj);
+            }
+            rdr.Close();
+            return itemlist;
+        }
+
+
+
+
+
+        public static int CUDProduct(SqlConnection dbcon, string CUDAction, Product obj)
+        {
+            SqlCommand cmd = new SqlCommand();
+            if (CUDAction == "create")
+            {
+                cmd.CommandText = "insert into Products " +
+                "Values (@ProductID,@Name,@ShortDescription,@LongDescription,@CategoryID,@ImageFile,@UnitPrice,@OnHand)";
+                //copy parameter assignment statements here
+                cmd.Parameters.AddWithValue("@ProductID", SqlDbType.VarChar).Value = obj.ProductID;
+                cmd.Parameters.AddWithValue("@Name", SqlDbType.VarChar).Value = obj.Name;
+                cmd.Parameters.AddWithValue("@ShortDescription", SqlDbType.VarChar).Value = obj.ShortDescription;
+                cmd.Parameters.AddWithValue("@LongDescription", SqlDbType.VarChar).Value = obj.LongDescription;
+                cmd.Parameters.AddWithValue("@CategoryID", SqlDbType.VarChar).Value = obj.CategoryID;
+                cmd.Parameters.AddWithValue("@ImageFile", SqlDbType.VarChar).Value = obj.ImageFile;
+                cmd.Parameters.AddWithValue("@UnitPrice", SqlDbType.Decimal).Value = obj.UnitPrice;
+                cmd.Parameters.AddWithValue("@OnHand", SqlDbType.Int).Value = obj.OnHand;
+            }
+            else if (CUDAction == "update")
+            {
+                cmd.CommandText = "update Products set Name = @Name,ShortDescription = @ShortDescription,LongDescription = @LongDescription,CategoryID = @CategoryID,ImageFile = @ImageFile,UnitPrice = @UnitPrice,OnHand = @OnHand Where ProductID = @ProductID";
+                //copy parameter assignment statements here
+                cmd.Parameters.AddWithValue("@ProductID", SqlDbType.VarChar).Value = obj.ProductID;
+                cmd.Parameters.AddWithValue("@Name", SqlDbType.VarChar).Value = obj.Name;
+                cmd.Parameters.AddWithValue("@ShortDescription", SqlDbType.VarChar).Value = obj.ShortDescription;
+                cmd.Parameters.AddWithValue("@LongDescription", SqlDbType.VarChar).Value = obj.LongDescription;
+                cmd.Parameters.AddWithValue("@CategoryID", SqlDbType.VarChar).Value = obj.CategoryID;
+                cmd.Parameters.AddWithValue("@ImageFile", SqlDbType.VarChar).Value = obj.ImageFile;
+                cmd.Parameters.AddWithValue("@UnitPrice", SqlDbType.Decimal).Value = obj.UnitPrice;
+                cmd.Parameters.AddWithValue("@OnHand", SqlDbType.Int).Value = obj.OnHand;
+            }
+            else if (CUDAction == "delete")
+            {
+                cmd.CommandText = "delete Products where ProductID = @ProductID";
+                cmd.Parameters.AddWithValue("@ProductID", SqlDbType.VarChar).Value = obj.ProductID;
+            }
+            cmd.Connection = dbcon;
+            int intResult = cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            return intResult;
+        }
+
+
+    }
+}
